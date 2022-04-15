3241d7c5e162381962f0608191b4bf9b90ced91c
Keitaroh Kobayashi <keita@kbys.me>
2022-04-15 10:23:57 +0900

    Start the tilehash with a `-` for negative tiles, restore 2**25 tilespace
    Also uses 1-8 instead of 0-7 to avoid tilehashes that start with 0

--- docs/index.es.js
+++ docs/index.es.js
@@ -1,3 +1,4 @@
+const ZFXY_1M_ZOOM_BASE = 25;
 function getParent$1(tile) {
     const [f, x, y, z] = tile;
     return [f >> 1, x >> 1, y >> 1, z - 1];
@@ -17,26 +18,35 @@ function getChildren$1(tile) {
 }
 function generate$1(tile) {
     let [f, x, y, z] = tile;
+    const originalF = f;
     let out = '';
     while (z > 0) {
-        const thisTile = [f, x, y, z];
+        const thisTile = [Math.abs(f), x, y, z];
         const parent = getParent$1(thisTile);
         const childrenOfParent = getChildren$1(parent);
-        const positionInParent = childrenOfParent.findIndex(([xf, xx, xy, xz]) => xf === f && xx === x && xy === y && xz === z);
-        out = positionInParent.toString(8) + out;
+        const positionInParent = childrenOfParent.findIndex(([xf, xx, xy, xz]) => xf === Math.abs(f) && xx === x && xy === y && xz === z);
+        out = (positionInParent + 1).toString() + out;
         f = parent[0];
         x = parent[1];
         y = parent[2];
         z = parent[3];
     }
-    return out;
+    return (originalF < 0 ? '-' : '') + out;
 }
 function parse$1(th) {
+    let negativeF = false;
+    if (th[0] === '-') {
+        negativeF = true;
+        th = th.substring(1);
+    }
     let children = getChildren$1([0, 0, 0, 0]);
     let lastChild;
     for (const c of th) {
-        lastChild = children[parseInt(c, 8)];
+        lastChild = children[parseInt(c, 10) - 1];
         children = getChildren$1(lastChild);
+    }
+    if (negativeF) {
+        lastChild[0] = -lastChild[0];
     }
     return lastChild;
 }
@@ -53,10 +63,10 @@ function toURL$1(input) {
 }
 function calculateZFXY(input) {
     const meters = typeof input.elevation !== 'undefined' ? input.elevation : 0;
-    if (meters <= -(2 ** 24) || meters >= (2 ** 24)) {
-        throw new Error(`ZFXY only supports elevation between -2^24 and +2^24.`);
+    if (meters <= -(2 ** ZFXY_1M_ZOOM_BASE) || meters >= (2 ** ZFXY_1M_ZOOM_BASE)) {
+        throw new Error(`ZFXY only supports elevation between -2^${ZFXY_1M_ZOOM_BASE} and +2^${ZFXY_1M_ZOOM_BASE}.`);
     }
-    const f = Math.floor(((2 ** input.zoom) * meters) / (2 ** 25)) + (2 ** (input.zoom - 1));
+    const f = Math.floor(((2 ** input.zoom) * meters) / (2 ** ZFXY_1M_ZOOM_BASE));
     // Algorithm adapted from tilebelt.js
     const d2r = Math.PI / 180;
     const sin = Math.sin(input.lat * d2r);
@@ -77,6 +87,7 @@ function calculateZFXY(input) {
 
 var zfxyFuncs = /*#__PURE__*/Object.freeze({
   __proto__: null,
+  ZFXY_1M_ZOOM_BASE: ZFXY_1M_ZOOM_BASE,
   getParent: getParent$1,
   getChildren: getChildren$1,
   generate: generate$1,
--- src/zfxy.ts
+++ src/zfxy.ts
@@ -1,5 +1,7 @@
 export type ZFXYTile = [number, number, number, number];
 
+export const ZFXY_1M_ZOOM_BASE = 25 as const;
+
 export function getParent(tile: ZFXYTile): ZFXYTile {
   const [f,x,y,z] = tile;
   return [f>>1,x>>1,y>>1,z-1];
@@ -20,29 +22,38 @@ export function getChildren(tile: ZFXYTile): ZFXYTile[] {
 
 export function generate(tile: ZFXYTile): string {
   let [f,x,y,z] = tile;
+  const originalF = f;
   let out = '';
   while (z>0) {
-    const thisTile: ZFXYTile = [f,x,y,z];
+    const thisTile: ZFXYTile = [Math.abs(f),x,y,z];
     const parent = getParent(thisTile);
     const childrenOfParent = getChildren(parent);
     const positionInParent = childrenOfParent.findIndex(
-      ([xf, xx, xy, xz]) => xf === f && xx === x && xy === y && xz === z
+      ([xf, xx, xy, xz]) => xf === Math.abs(f) && xx === x && xy === y && xz === z
     );
-    out = positionInParent.toString(8) + out;
+    out = (positionInParent + 1).toString() + out;
     f = parent[0];
     x = parent[1];
     y = parent[2];
     z = parent[3];
   }
-  return out;
+  return (originalF < 0 ? '-' : '') + out;
 }
 
 export function parse(th: string): ZFXYTile {
+  let negativeF = false;
+  if (th[0] === '-') {
+    negativeF = true;
+    th = th.substring(1);
+  }
   let children = getChildren([0,0,0,0]);
   let lastChild: ZFXYTile;
   for (const c of th) {
-    lastChild = children[parseInt(c, 8)];
+    lastChild = children[parseInt(c, 10) - 1];
     children = getChildren(lastChild);
+  }
+  if (negativeF) {
+    lastChild[0] = -lastChild[0];
   }
   return lastChild;
 }
@@ -67,10 +78,10 @@ export interface CalculateZFXYInput {
 
 export function calculateZFXY(input: CalculateZFXYInput): ZFXYTile {
   const meters = typeof input.elevation !== 'undefined' ? input.elevation : 0;
-  if (meters <= -(2**24) || meters >= (2**24)) {
-    throw new Error(`ZFXY only supports elevation between -2^24 and +2^24.`);
+  if (meters <= -(2**ZFXY_1M_ZOOM_BASE) || meters >= (2**ZFXY_1M_ZOOM_BASE)) {
+    throw new Error(`ZFXY only supports elevation between -2^${ZFXY_1M_ZOOM_BASE} and +2^${ZFXY_1M_ZOOM_BASE}.`);
   }
-  const f = Math.floor(((2 ** input.zoom) * meters) / (2 ** 25)) + (2 ** (input.zoom - 1));
+  const f = Math.floor(((2 ** input.zoom) * meters) / (2 ** ZFXY_1M_ZOOM_BASE));
 
   // Algorithm adapted from tilebelt.js
   const d2r = Math.PI / 180;
